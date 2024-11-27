d7804fc7f34397ba60ea4ca284de099770a95a64
NitheeshKumarReddy6203 <105546645+NitheeshKumarReddy6203@users.noreply.github.com>
2024-11-27 14:38:54 +0530

    Create test_calculator.py

---
+++ test_calculator.py
@@ -0,0 +1,26 @@
+import sys
+import os
+import unittest
+from ..src import calculator
+
+# Import the functions
+from calculator import add, subtract, multiply
+
+class TestCalculator(unittest.TestCase):
+    def test_add(self):
+        result = add(2, 3)
+        print(f"Addition result: {result}")
+        self.assertEqual(result, 5)
+
+    def test_subtract(self):
+        result = subtract(5, 3)
+        print(f"Subtraction result: {result}")
+        self.assertEqual(result, 2)
+
+    def test_multiply(self):
+        result = multiply(2, 3)
+        print(f"Multiplication result: {result}")
+        self.assertEqual(result, 6)
+
+if __name__ == '__main__':
+    unittest.main()
